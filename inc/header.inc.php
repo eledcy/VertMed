d354690db6af7694bb64c86d2e09031efd526f3a
eledcy <61116938+eledcy@users.noreply.github.com>
2020-04-21 20:01:55 +0100

    Add files via upload

---
+++ inc/header.inc.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>VertMed</title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    </head>
+    <body>
+        <div class="HeaderMenu">
+            <div id="wrapper">
+                <div class="logo">
+                    <img src="./img/logo.PNG" />
+                </div>
+                <div class="search_box">
+                    <form action="search.php" method="GET" id="search">
+                        <input type="text" name="q" size="60" placeholder="Search..." />
+                    </form>
+                </div>
+                <div id="menu">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Signup</a>
+                    <a href="#">Signin</a>
+                </div>
+            </div>
+        </div>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include ( "./inc/header.inc.php" ); ?>
+    <div id="wrapper">
+        <table>
+            <tr>
+                <td width="60%" valign="top">
+                    <h2>Join VertMed today</h2>
+                </td>
+                <td width="50%" valign="top">
+                    <h2>Signup Below!</h2>
+                    <form action="#" method="POST">
+                        <input type="text" name="fname" size="25" placeholder="First Name"><p> </p>
+                        <input type="text" name="lname" size="25" placeholder="Last Name"><p> </p>
+                        <input type="email" name="email" size="25" placeholder="Email"><p> </p>
+                        <input type="text" name="uname" size="25" placeholder="UserName"><p> </p>
+                        <input type="password" name="pass" size="25" placeholder="Password"><p> </p>
+                        <input type="password" name="cpass" size="25" placeholder="Confirm Password"><p> </p>
+                        <input type="submit" name="submit" value="SignUp">
+                    </form>
+                </td>
+            </tr>
+        </table>
+    </div>
+    </body>
+</html>
